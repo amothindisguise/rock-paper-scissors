41c5aa18f4f3d3f6b01031fe5b150b3dce978286
amothindisguise <mluisenteria@gmail.com>
2024-07-28 00:07:10 +0800

    Add js folder

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # rock-paper-scissors
+Project Title:
+
+Rock Paper Scissors
+
+Description:
+
+TOP Project where we are tasked to build a game called Rock Paper Scissors applying Javascipt Lesson.
+
+Created by:
+
+amothindisguise - mluisenteria@gmail.com
---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+//create a function named getComputerChoice()
+//that will randomly return rock, paper, or scissors
+//Hint: use Math.random()
+//create a new function called getHumanChoice() that takes
+//valid choices [rock, paper, scissors]
+//Track scores using variables: humanScore and computerScore
+//initialized to 0
+//create a new function called playRound() that
+//accepts 2 parameters, humanChoice and computerChoice
+//humanChoice parameter is case-insensitive
+//on playRound, prompt who won
+//five rounds
+//new function named playGame(), playRound()
+//and scores are declared inside playGame()
+
+//Press the button to start
+//Ask the user for an input (rock, paper, scissors)
+//validate the input
+//if input === rock || paper || scissors proceed
+//generate computerChoice
+//compare computerChoice to humanChoice
+//if === draw
+//else proceed to checking
+//else ask to input a valid answer

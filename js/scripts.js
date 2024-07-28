418f078686dfbc87b50e7fde0f97f3d139f0041d
amothindisguise <mluisenteria@gmail.com>
2024-07-29 00:13:13 +0800

    Done Rock Paper Scissors, To Add Error Catching

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,57 @@
-//create a function named getComputerChoice()
-//that will randomly return rock, paper, or scissors
-//Hint: use Math.random()
-//create a new function called getHumanChoice() that takes
-//valid choices [rock, paper, scissors]
-//Track scores using variables: humanScore and computerScore
-//initialized to 0
-//create a new function called playRound() that
-//accepts 2 parameters, humanChoice and computerChoice
-//humanChoice parameter is case-insensitive
-//on playRound, prompt who won
-//five rounds
-//new function named playGame(), playRound()
-//and scores are declared inside playGame()
+function getComputerChoice(choice) {
+    choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        return choice = "ROCK"; 
+    } else if (choice == 1) {
+        return choice = "PAPER";
+    } else {
+        return choice = "SCISSORS";
+    }
+}
 
-//Press the button to start
-//Ask the user for an input (rock, paper, scissors)
-//validate the input
-//if input === rock || paper || scissors proceed
-//generate computerChoice
-//compare computerChoice to humanChoice
-//if === draw
-//else proceed to checking
-//else ask to input a valid answer
+function getHumanChoice(choice) {
+    choice = prompt("Please choose between rock, paper, and scissors.");
+    return choice.toUpperCase();
+}
 
-function humanChoice(str) {
-    str = prompt("Please choose between rock, paper, and scissors");
-    alert(str);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let rounds = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+    
+        if(humanChoice === computerChoice) {
+            alert("Round " + rounds + "\nDraw! You both chose " + humanChoice );
+            alert("Your score: " + humanScore + "\nComputer's score: " + computerScore);
+        } else if (humanChoice === "ROCK" && computerChoice === "SCISSORS") {
+            alert("Round " + rounds + "\nYou win! Your " + humanChoice + " beats Computer's " + computerChoice + ".");
+            humanScore++;
+            alert("Your score: " + humanScore + "\nComputer's score: " + computerScore);
+        } else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
+            alert("Round " + rounds + "\nYou win! Your " + humanChoice + " beats Computer's " + computerChoice + ".");
+            humanScore++;
+            alert("Your score: " + humanScore + "\nComputer's score: " + computerScore);
+        } else if(humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+            alert("Round " + rounds + "\nYou win! Your " + humanChoice + " beats Computer's " + computerChoice + ".");
+            humanScore++;
+            alert("Round " + rounds + "\nYour score: " + humanScore + "\nComputer's score: " + computerScore);
+        } else {
+            alert("Round " + rounds + "\nYou lose! Computer's " + computerChoice + " beat your " + humanChoice + ".");
+            computerScore++;
+            alert("Your score: " + humanScore + "\nComputer's score: " + computerScore);
+        }
+    }
+
+    for (rounds = 1; rounds <= 5; rounds++) {
+        playRound();
+    }
+
+    if (humanScore == computerScore) {
+        alert("It's a DRAW! Your score: " + humanScore + "," + " Computer's score: " + computerScore + ".");
+    } else if (humanScore > computerScore) {
+        alert("You WIN the game! Your score is " + humanScore + "while Computer's score is " + computerScore + ".");
+    } else alert("You LOSE! Computer's score " + computerScore + " beat your score " + humanScore + ".");
 }
